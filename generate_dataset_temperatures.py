1d14222754124dff60c54d5d52c7139d1e49b68c
gboxo <gerardboxocorominas@gmail.com>
2024-10-01 14:07:17 +0000

    Greater temperature

--- generate_dataset_temperatures.py
+++ generate_dataset_temperatures.py
@@ -1,24 +1,6 @@
 from sae_lens import HookedSAETransformer, SAE, SAEConfig
-from gemma_utils import get_gemma_2_config, gemma_2_sae_loader
-import numpy as np
 import torch
 from tqdm import tqdm
-import einops
-import re
-from jaxtyping import Int, Float
-from typing import List, Optional, Any
-from torch import Tensor
-import json
-import os
-from torch.utils.data import Dataset, DataLoader
-import random
-import seaborn as sns
-import matplotlib.pyplot as plt
-from collections import defaultdict
-import random
-from transformer_lens.utils import get_act_name
-from IPython.display import display, HTML
-import plotly.express as px
 
 
 
@@ -26,12 +8,12 @@ import plotly.express as px
 
 
 
-def sample(model,input_ids):
+def sample(model,input_ids,temperature):
     tokens = model.to_tokens(input_ids, prepend_bos=False)
     out = model.generate(
         tokens,
         max_new_tokens = 100,
-        temperature = 0.7,
+        temperature = temperature,
         top_p = 0.8,
         stop_at_eos=True,
         verbose=False,
@@ -43,20 +25,24 @@ def sample(model,input_ids):
 
 def generate_lists(topics):
     generation_dict = {}
+    
 
-    for topic in tqdm(topics):
-        generation_dict[topic] = [] 
-        for _ in range(5):
-            messages = [
-                {"role": "user", "content": f"Provide me with a long list of a many {topic}. Just provide the names, no need for any other information."},
-            ]
-            input_ids = model.tokenizer.apply_chat_template(messages, tokenize=False, add_generation_prompt=True)
 
-            input_ids += "-"
-            out = sample(model,input_ids)
+    for temp in [0.1,0.5,0.7,0.9,1.3]:
+        generation_dict[temp] = {}
+        for topic in tqdm(topics):
+            generation_dict[temp][topic] = [] 
+            for _ in range(5):
+                messages = [
+                    {"role": "user", "content": f"Provide me with a short list with a few {topic}. Just provide the names, no need for any other information."},
+                ]
+                input_ids = model.tokenizer.apply_chat_template(messages, tokenize=False, add_generation_prompt=True)
 
-            toks = out.detach().clone()
-            generation_dict[topic].append(toks)
+                input_ids += "-"
+                out = sample(model,input_ids,temp)
+
+                toks = out.detach().clone()
+                generation_dict[temp][topic].append(toks)
     return generation_dict
 
 if __name__ == "__main__":
@@ -68,4 +54,4 @@ if __name__ == "__main__":
     "Inventions","Languages","Capital Cities","Movies","Books","TV Shows",
     "Famous Scientists","Famous Writers","Video Games","Companies","Colors"]
     generation_dict = generate_lists(topics)
-    torch.save(generation_dict, "gemma2_generation_long_dict.pt")
+    torch.save(generation_dict, "gemma2_generation_temps_dict.pt")
