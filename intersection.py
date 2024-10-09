06e4ebaff7311d17a9d963579e9578d65e271fa9
gboxo <gboxocor7@alumnes.ub.edu>
2024-10-09 23:35:01 +0200

    expression matrix

--- intersection.py
+++ intersection.py
@@ -1,91 +1,60 @@
 
-"""
-
-def compute_top_k_feature_intersection(model,toks, saes_dict, k:int = 10):
-    feature_attribution_df = calculate_feature_attribution(
-        model = model,
-        input = toks,
-        metric_fn = metric_fn,
-        include_saes=saes_dict,
-        include_error_term=True,
-        return_logits=True,
-    )
-    all_df_dict = defaultdict(dict) 
-    for attrb_pos,(tok1,tok2) in zip([46,48],[(blanck_tok_id,break_tok_id),(eot_tok_id,hypen_tok_id)]):
-        for i,func in enumerate([metric_fn, metric_fn_log_prob]):
-            if i == 0:
-                func = partial(func, pos=attrb_pos, tok0 = tok1, tok1 = tok2)
-                metric_name = "loggit_diff"
-            else:
-                func = partial(func, pos=attrb_pos,tok_id = tok1)
-                metric_name = "log_prob"
-            feature_attribution_df = calculate_feature_attribution(
-                model = model,
-                input = toks,
-                metric_fn = func,
-                include_saes=saes_dict,
-                include_error_term=True,
-                return_logits=True,
-            )
-            all_tup = []
-            for key in saes_dict.keys():
-                df_long_nonzero = convert_sparse_feature_to_long_df(feature_attribution_df.sae_feature_attributions[key][0])
-                df_long_nonzero.sort_values("attribution", ascending=True)
-                df_long_nonzero = df_long_nonzero.nlargest(50, "attribution")
-                tuple_list = [(pos,feat) for pos,feat in zip(df_long_nonzero["position"],df_long_nonzero["feature"])]
-                all_tup.append(tuple_list)
-            all_df_dict[metric_name][tok1] = all_tup
-    return all_df_dict
-
-tups0 = all_df_dict["loggit_diff"][blanck_tok_id]
-tups1 = all_df_dict["loggit_diff"][eot_tok_id]
-tups2 = all_df_dict["log_prob"][blanck_tok_id]
-tups3 = all_df_dict["log_prob"][eot_tok_id]
-
-# Compute the proportion of intersection
-def compute_proportion_intersection(tups0,tups1,use_position = False):
-    intersect01 = []
-    for tup0,tup1 in zip(tups0,tups1):
-        if use_position:
-            intersec = set(tup0).intersection(set(tup1))
-        else:
-            # just use the feature ids
-            intersec = set([feat for pos,feat in tup0]).intersection(set([feat for pos,feat in tup1])) 
-        intersect01.append(len(intersec)/len(tup0))
-    return np.mean(intersect01)
-
-
-
-intersect01 = compute_proportion_intersection(tups0,tups1, use_position = True)
-intersect02 = compute_proportion_intersection(tups0,tups2, use_position = True)
-intersect03 = compute_proportion_intersection(tups0,tups3, use_position = True)
-intersect12 = compute_proportion_intersection(tups1,tups2, use_position = True)
-intersect13 = compute_proportion_intersection(tups1,tups3, use_position = True)
-intersect23 = compute_proportion_intersection(tups2,tups3, use_position = True)
-
-intersect_mat = np.zeros((4,4))
-intersect_mat[0,1] = intersect01
-intersect_mat[0,2] = intersect02
-intersect_mat[0,3] = intersect03
-intersect_mat[1,2] = intersect12
-intersect_mat[1,3] = intersect13
-intersect_mat[2,3] = intersect23
-np.fill_diagonal(intersect_mat, 1)
-print(intersect_mat)
+import torch
+from collections import defaultdict, Counter
+import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
-sns.heatmap(intersect_mat, annot=True)
-plt.show()
+from collections import defaultdict
 
+# %%
+from  rich.console import Console 
+from rich.table import Table 
 
+def display_table(dataframe):
+    table = Table(show_header=True, header_style="bold magenta")
+    for column in dataframe.columns:
+        table.add_column(column)
+    
+    for index, row in dataframe.iterrows():
+        table.add_row(*[str(value) for value in row])
+    
+    console = Console()
+    console.print(table)
 
-"""
+def display_heatmap(x,comp):
 
+# Show the features-presence heatmap
+    topic_feat_dict = defaultdict(lambda: defaultdict(int))
+    for topic, topic_dict in x.items():
+        for eg, features_dict in topic_dict.items():
+            for pos, feat, layer in features_dict:
+                if layer == comp:
+                    topic_feat_dict[topic][feat] += 1
+
+    unique_features = sorted(set(feat for topic in topic_feat_dict for feat in topic_feat_dict[topic].keys()))
+    heatmap_data = pd.DataFrame(0, index=topic_feat_dict.keys(), columns=unique_features)
+
+    for topic, features in topic_feat_dict.items():
+        for feat, count in features.items():
+            heatmap_data.at[topic, feat] = 1 if count > 0 else 0
+
+# Sort the heatmap data by the number of active features
+    heatmap_data = heatmap_data[heatmap_data.sum(axis=0).sort_values(ascending=False).index]
+# Calculate the number of unique features
+    num_features = heatmap_data.shape[1]
+
+# Adjust the figure size based on the number of features
+    plt.figure(figsize=((12, 8)))  # Width is proportional to the number of features
+    sns.heatmap(heatmap_data, annot=False, cmap="YlGnBu", cbar=False, linewidths=0.5, linecolor='black')
+
+    plt.title("Feature Presence Heatmap")
+    plt.xlabel("Unique Features")
+    plt.ylabel("Topics")
+    plt.xticks(rotation=45, ha='right')  # Rotate x-axis labels for better readability
+    plt.show()
 
 
 # %%
-import torch
-from collections import defaultdict, Counter
 
 x = torch.load("all_tuples_dict_top_100_item_pos_log_prob_all_attn.pt")
 
@@ -108,27 +77,50 @@ for key in topic_features.keys():
     for layer in top_feats.keys():
         all_tuples_df.append({"Topic": key, "Layer": layer, "Top-10 feats": top_feats[layer], "Top-10 positions": top_positions[layer]})
 
-import pandas as pd
 all_tuples_df = pd.DataFrame(all_tuples_df)
 all_tuples_df.to_html("all_tuples_df.html")
 
 
 
-# %%
-from  rich.console import Console 
-from rich.table import Table 
-
-def display_table(dataframe):
-    table = Table(show_header=True, header_style="bold magenta")
-    for column in dataframe.columns:
-        table.add_column(column)
-    
-    for index, row in dataframe.iterrows():
-        table.add_row(*[str(value) for value in row])
-    
-    console = Console()
-    console.print(table)
-
-
 display_table(all_tuples_df)
 
+
+for l in [2,7,14,22]:
+    display_heatmap(x,f"blocks.{l}.attn.hook_z")
+
+
+
+# ======== Expression matrix ======
+total_examples = 0 
+unique_features = []
+for key,val in x.items():
+    for eg_id, tups in val.items():
+        total_examples += 1
+        for pos, feat, layer in tups:
+            if layer == "blocks.2.attn.hook_z":
+                unique_features.append(feat)
+unique_features = list(set(unique_features))
+
+count_matrix = torch.zeros((len(unique_features), total_examples))
+
+
+i = 0
+for (key, val) in x.items():
+    for eg_id, tups in val.items():
+        for pos, feat, layer in tups:
+            if layer == "blocks.2.attn.hook_z":
+                count_matrix[unique_features.index(feat), i] += 1
+        i+=1
+
+
+
+df = pd.DataFrame(count_matrix/50, columns=range(total_examples), index=unique_features)
+# remove nan and inf values
+df.fillna(0, inplace=True)
+df.replace([float('inf')], 0, inplace=True)
+
+# Create a clustermap with hierarchical clustering
+plt.figure(figsize=(14, 10))
+sns.clustermap(df, cmap='RdYlGn', standard_scale=1, figsize=(12, 12), cbar_kws={'label': 'Expression Level'})
+plt.title('Hierarchical Clustering of Gene Expression Data')
+plt.show()
