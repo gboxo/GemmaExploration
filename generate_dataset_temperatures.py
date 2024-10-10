30f9b912a35d848665150510e685f8ed76a4e2dc
gboxo <gerardboxocorominas@gmail.com>
2024-10-10 14:44:22 +0000

    geneate

--- generate_dataset_temperatures.py
+++ generate_dataset_temperatures.py
@@ -3,6 +3,11 @@ import logging
 import torch
 from tqdm import tqdm
 
+from sae_lens import HookedSAETransformer, SAE, SAEConfig
+import logging
+import torch
+from tqdm import tqdm
+
 # Configure logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
@@ -24,17 +29,17 @@ def sample(model,input_ids,temperature):
 
 def generate_lists(topics):
     temps = [0.1,0.5,0.7,0.9,1.3]
-    temps = [0.1,1.3]
+    temps = [0.8,0.9]
 
     generation_dict = {}
-    for temp in tqdm(temps):
+    for temp in tqdm(temps, desc="Generating lists", unit="temperature"):
         generation_dict[temp] = {}
         for topic in topics:
             generation_dict[temp][topic] = [] 
-            for generation in range(2):
+            for generation in range(5):
                 logging.info(f"Generating for topic '{topic}' at temperature {temp}. Generation {generation + 1}/5")
                 messages = [
-                    {"role": "user", "content": f"Provide me with a short list of {topic}. Just provide the names, no need for any other information."},
+                    {"role": "user", "content": f"Provide me with a list of {topic}. Just provide the names, no need for any other information."},
                 ]
                 input_ids = model.tokenizer.apply_chat_template(messages, tokenize=False, add_generation_prompt=True)
 
@@ -53,6 +58,6 @@ if __name__ == "__main__":
     "Fruits","Vegetables","Car Brands","Sports","Rivers","Mountains","Ocean",
     "Inventions","Languages","Capital Cities","Movies","Books","TV Shows",
     "Famous Scientists","Famous Writers","Video Games","Companies","Colors"]
-    topics = ["City Names","Countries","Animals"]
+    topics = ["Languages","Countries","Animals","Books"]
     generation_dict = generate_lists(topics)
-    torch.save(generation_dict, "gemma2_generation_temps_dict_v0.pt")
+    torch.save(generation_dict, "gemma2_generation_temps_dict_selection_no_short.pt")
